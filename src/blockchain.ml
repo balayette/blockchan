51f15406401bc10fed3b15fbbe6139ece2c0a6be
Nicolas Manichon <nmanichon@gmail.com>
2017-07-26 18:05:41 +0200

    Removed redundant calls

--- src/blockchain.ml
+++ src/blockchain.ml
@@ -24,20 +24,25 @@ let new_blockchain () =
 (* TODO : Actual error handling *)
 let add_block bl b =
   let last_block = List.hd (get_blocks bl) in
-  if (Block.get_hash last_block) <> (Block.get_previous_hash b) then (
-    Printf.printf "The block with hash %s doesn't match the previous block : expected prev_hash = %s, got prev_hash = %s\n" (Block.get_hash b) (Block.get_hash last_block) (Block.get_previous_hash b); None
+  let last_hash = (Block.get_hash last_block)
+  and curr_hash = (Block.get_hash b)
+  and prev_hash = (Block.get_previous_hash b)
+  and curr_id = (Block.get_id b)
+  and last_id = (Block.get_id last_block) in
+  if last_hash <> prev_hash then (
+    Printf.printf "The block with hash %s doesn't match the previous block : expected prev_hash = %s, got prev_hash = %s\n" curr_hash last_hash prev_hash; None
   )
   else (
-    if (Block.get_id b) <> (Block.get_id last_block) + 1 then (
-      Printf.printf "The block with hash %s doesn't match the previous block : expected id = %d, got id = %d" (Block.get_hash b) ((Block.get_id last_block) + 1) (Block.get_id b); None
+    if curr_id <> last_id + 1 then (
+      Printf.printf "The block with hash %s doesn't match the previous block : expected id = %d, got id = %d" curr_hash (last_id + 1) curr_id; None
     )
     else (
-      let actual_hash = Block.block_hash (Block.get_timestamp b) (Block.get_transactions b) (Block.get_id b) in
-      if actual_hash <> (Block.get_hash b) then (
-        Printf.printf "The block has hash %s, expected %s" (Block.get_hash b) actual_hash; None
+      let actual_hash = Block.block_hash (Block.get_timestamp b) (Block.get_transactions b) curr_id in
+      if actual_hash <> curr_hash then (
+        Printf.printf "The block has hash %s, expected %s" curr_hash actual_hash; None
       )
       else (
-        Printf.printf "The block with hash %s is valid, adding it to the blockchain\n" (Block.get_hash b);
+        Printf.printf "The block with hash %s is valid, adding it to the blockchain\n" curr_hash;
         Some {count = bl.count + 1; blocks = b::bl.blocks}
       )
     )
